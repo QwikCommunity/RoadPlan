0e3e71b49c5318f87069b9c1177f8c8d513dc907
gioboa <giorgiob.boa@gmail.com>
2023-11-03 17:49:28 +0100

    chore: aria labels

--- src/components/Icons/CloseIcon.tsx
+++ src/components/Icons/CloseIcon.tsx
@@ -3,11 +3,12 @@ import { component$ } from "@builder.io/qwik";
 export const CloseIcon = component$(() => {
   return (
     <svg
+      xmlns="http://www.w3.org/2000/svg"
       class="h-10 w-10 rounded-xl stroke-black p-1 dark:stroke-white"
       fill="none"
       stroke-width="2.5"
       viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
+      aria-label="Close"
     >
       <path
         d="M6 18L18 6M6 6l12 12"
