e3381db35e863ed7511832471fd6a37b5ef31d3b
gioboa <giorgiob.boa@gmail.com>
2023-11-03 17:04:58 +0100

    feat: icons

--- src/components/Icons/CloseIcon.tsx
+++ src/components/Icons/CloseIcon.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik";
 export const CloseIcon = component$(() => {
   return (
     <svg
-      class="mt-2 h-10 w-10 rounded-xl stroke-black p-1 dark:stroke-white"
+      class="h-10 w-10 rounded-xl stroke-black p-1 dark:stroke-white"
       fill="none"
       stroke-width="2.5"
       viewBox="0 0 24 24"
