68c7a387a35755489303680420e7ae2a7f848ae0
gioboa <giorgiob.boa@gmail.com>
2023-11-17 22:32:31 +0100

    refactor: RoadPlan CLI

--- cli/src/templates/RoadPlan/src/components/extras/HeroBanner/HeroBanner.tsx
+++
@@ -1,9 +0,0 @@
-import { component$ } from "@builder.io/qwik";
-
-export const HeroBanner = component$<{ title: string }>(({ title }) => {
-  return (
-    <header class="hero-banner">
-      <h1 class="hero-title">{title}</h1>
-    </header>
-  );
-});
