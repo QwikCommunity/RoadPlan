01b7ee187fdd0ae08f0bf6e781ecd4be3182b157
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2023-11-20 20:58:14 +0100

    feat: add git initialisation step (#9)

--- packages/create-roadplan/index.ts
+++ packages/create-roadplan/index.ts
@@ -1,15 +1,69 @@
 #! /usr/bin/env node
 
-import { cancel, confirm, intro, isCancel, log, outro, text } from "@clack/prompts";
-import { exec, spawn } from "child_process";
+import { cancel, confirm, intro, isCancel, log, outro, spinner, text } from "@clack/prompts";
+import { ChildProcess, exec, spawn } from "child_process";
 import { cpSync, existsSync, mkdirSync } from "fs";
-import { gray } from 'kleur/colors';
-import path from "path";
+import { gray, red } from 'kleur/colors';
+import fs from 'node:fs';
+import os from 'node:os';
+import { join } from 'node:path';
+import path, { resolve } from "path";
 import { fileURLToPath } from "url";
 import detectPackageManager from 'which-pm-runs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+export function resolveRelativeDir(dir: string) {
+  // check if the outDir start with home ~
+  if (dir.startsWith('~/')) {
+    return resolve(os.homedir(), dir);
+  } else {
+    return resolve(process.cwd(), dir);
+  }
+}
+
+export function runCommand(cmd: string, args: string[], cwd: string) {
+  let child: ChildProcess;
+
+  const install = new Promise<boolean>((resolve) => {
+    try {
+      child = spawn(cmd, args, {
+        cwd,
+        stdio: 'ignore',
+      });
+
+      child.on('error', (e) => {
+        if (e) {
+          if (e.message) {
+            log.error(red(String(e.message)) + `\n\n`);
+          } else {
+            log.error(red(String(e)) + `\n\n`);
+          }
+        }
+        resolve(false);
+      });
+
+      child.on('close', (code) => {
+        if (code === 0) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    } catch (e) {
+      resolve(false);
+    }
+  });
+
+  const abort = async () => {
+    if (child) {
+      child.kill('SIGINT');
+    }
+  };
+
+  return { abort, install };
+}
 
 export const isPackageManagerInstalled = (packageManager) => {
   return new Promise((resolve) => {
@@ -85,6 +139,8 @@ const createProject = async () => {
     }
     cpSync(templatePath, projectNameAnswer, { recursive: true });
 
+    const outDir: string = resolveRelativeDir(projectNameAnswer.trim());
+
     const runDepInstallAnswer = await confirm({
       message: `Would you like to install ${packageManager} dependencies?`,
       initialValue: true,
@@ -95,8 +151,39 @@ const createProject = async () => {
       process.exit(0);
     }
 
+    const gitInitAnswer = await confirm({
+      message: `Initialize a new git repository?`,
+      initialValue: true,
+    });
+
     log.step("Installing dependencies...");
     await installDependencies(packageManager, projectNameAnswer);
+
+    const s = spinner();
+
+    if (gitInitAnswer) {
+      if (fs.existsSync(join(outDir, '.git'))) {
+        log.info(`Git has already been initialized before. Skipping...`);
+      } else {
+        s.start('Git initializing...');
+  
+        try {
+          const res = [];
+          res.push(await runCommand('git', ['init'], outDir).install);
+          res.push(await runCommand('git', ['add', '-A'], outDir).install);
+          res.push(await runCommand('git', ['commit', '-m', 'Initial commit ⚡️'], outDir).install);
+  
+          if (res.some((r) => r === false)) {
+            throw '';
+          }
+  
+          s.stop('Git initialized 🎲');
+        } catch (e) {
+          s.stop('Git failed to initialize');
+          log.error(red(`Git failed to initialize. You can do this manually by running: git init`));
+        }
+      }
+    }
 
     outro("RoadPlan starter creation completed successfully!");
   } catch (err) {
