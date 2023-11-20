f5457d3f8b997609d32132b9ef5629742d5e07c9
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2023-11-20 14:46:05 +0100

    fix build

--- packages/create-roadplan/lib/template/roadplan.config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-    versionSelector: true,
-    title: "Fastify",
-    tagline: "Fast and low overhead web framework, for Node.js",
-    GitHub: "https://api.github.com/repos/fastify/fastify",
-};
