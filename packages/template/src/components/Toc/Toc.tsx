3623d7da2ba9337ccf769f043dc68f05fe43fc04
Benjamin <79079152+JuicyBenjamin@users.noreply.github.com>
2024-04-20 16:52:14 +0200

    Scroll behavior (#21)
    * chore(Toc): remove scroll behavior on hover
    
    * feat: add global scroll behavior smooth

--- packages/template/src/components/Toc/Toc.tsx
+++ packages/template/src/components/Toc/Toc.tsx
@@ -1,9 +1,9 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { useContent } from "@builder.io/qwik-city";
 
 export const Toc = component$(() => {
   const { headings } = useContent();
-  const lastScrollIdSig = useSignal("");
+
   return (
     <div class="fixed flex w-full flex-col px-6 pt-28 text-xl text-black dark:text-white">
       <span class="pb-4 pl-4">On this page</span>
@@ -11,15 +11,6 @@ export const Toc = component$(() => {
         <ul
           key={idx}
           class="border-l-[4px] py-1 pl-4 hover:border-gray-300 hover:dark:border-gray-500"
-          onMouseOver$={() => {
-            if (lastScrollIdSig.value !== id) {
-              const el = document.querySelector(`#${id}`);
-              if (el) {
-                el.scrollIntoView({ behavior: "smooth" });
-                lastScrollIdSig.value = id;
-              }
-            }
-          }}
         >
           <li class="text-xs hover:text-gray-500">
             <a href={`#${id}`}>{text}</a>
