7a89a8ca05130941913102141ffae4b46fc1a888
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-05-24 14:01:53 +0200

    chore: export all components

--- packages/roadplan-ui/src/index.ts
+++ packages/roadplan-ui/src/index.ts
@@ -1,1 +1,30 @@
 export { Button } from "./components/Button/Button";
+
+// export docs-components
+export { Aside } from "./docs-components/Aside/Aside";
+export { Footer } from "./docs-components/Footer/Footer";
+export { Header } from "./docs-components/Header/Header";
+export { ThemeSelector } from "./docs-components/ThemeSelector/ThemeSelector";
+export { Toc } from "./docs-components/Toc/Toc";
+export { VersionSelector } from "./docs-components/VersionSelector/VersionSelector";
+
+// export docs-layout
+export { default as ClassicLayout } from "./docs-layout/classic-layout";
+
+// export Icons
+export { CheckIcon } from "./Icons/CheckIcon";
+export { ChevronIcon } from "./Icons/ChevronIcon";
+export { CloseIcon } from "./Icons/CloseIcon";
+export { CopyIcon } from "./Icons/CopyIcon";
+export { ForkIcon } from "./Icons/ForkIcon";
+export { GitHubIcon } from "./Icons/GitHubIcon";
+export { MenuIcon } from "./Icons/MenuIcon";
+export { StarIcon } from "./Icons/StarIcon";
+export { ThemeDarkIcon } from "./Icons/ThemeDarkIcon";
+export { ThemeLightIcon } from "./Icons/ThemeLightIcon";
+
+// export user-components
+export { CodeSnippet } from "./user-components/CodeSnippet/CodeSnippet";
+export { CopyButton } from "./user-components/CopyButton/CopyButton";
+export { Highlight } from "./user-components/Highlight/Highlight";
+export { components } from "./user-components/MdxComponents/MdxComponents";
