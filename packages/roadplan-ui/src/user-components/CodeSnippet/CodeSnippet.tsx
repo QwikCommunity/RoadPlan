02e7d8a74fedf1853660b6ece34ae376b3d52fb1
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-05-24 12:29:24 +0200

    feat(user-components): add components to within the docs site

---
+++ packages/roadplan-ui/src/user-components/CodeSnippet/CodeSnippet.tsx
@@ -0,0 +1,46 @@
+import type { PropsOf } from "@builder.io/qwik";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
+import { isDev } from "@builder.io/qwik/build";
+import { Highlight } from "../Highlight/Highlight";
+
+// The below `/src/routes/docs/**/**/snippets/*.tsx` pattern is here so that import.meta.glob works both for styled and headless routes.
+// For example:
+// /src/routes/docs/components/styled/modal/snippets/building-blocks.tsx
+// /src/routes/docs/components/headless/modal/snippets/building-blocks.tsx
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const codeSnippets: any = import.meta.glob(`/src/**/snippets/*`, {
+  query: "?raw",
+  eager: isDev ? false : true,
+});
+
+type CodeSnippetProps = PropsOf<"div"> & {
+  name: string;
+};
+
+export const CodeSnippet = component$<CodeSnippetProps>(({ name }) => {
+  const location = useLocation();
+
+  // Determine the file extension if not specified
+  const fileExtension =
+    name.endsWith(".tsx") || name.endsWith(".ts") || name.endsWith(".css")
+      ? ""
+      : ".tsx";
+  const snippetPath = `/src/routes${location.url.pathname}snippets/${name}${fileExtension}`;
+
+  const codeSnippetSig = useSignal<string>();
+
+  useTask$(async () => {
+    const code = isDev
+      ? await codeSnippets[snippetPath]() // We need to call `await codeSnippets[snippetPath]()` in development as it is `eager:false`
+      : codeSnippets[snippetPath]; // We need to directly access the `codeSnippets[snippetPath]` expression in preview/production as it is `eager:true`
+    codeSnippetSig.value = code.default;
+  });
+
+  return (
+    <div class="shadow-3xl mb-6 rounded-md border shadow-lg">
+      <Highlight code={codeSnippetSig.value || ""} />
+    </div>
+  );
+});
---
+++ packages/roadplan-ui/src/user-components/CopyButton/CopyButton.tsx
@@ -0,0 +1,40 @@
+import type { QwikIntrinsicElements } from "@builder.io/qwik";
+import { $, component$, useSignal, useTask$ } from "@builder.io/qwik";
+import { CheckIcon } from "../../Icons/CheckIcon";
+import { CopyIcon } from "../../Icons/CopyIcon";
+
+type CopyButtonProps = QwikIntrinsicElements["button"] & {
+  code: string;
+  src?: string;
+};
+export async function copyToClipboardWithMeta(value: string) {
+  navigator.clipboard.writeText(value);
+}
+
+export const CopyButton = component$<CopyButtonProps>(({ code, ...props }) => {
+  const hasCopied = useSignal(false);
+
+  useTask$(({ track }) => {
+    track(() => hasCopied.value);
+    setTimeout(
+      $(() => {
+        hasCopied.value = false;
+      }),
+      2000,
+    );
+  });
+
+  return (
+    <button
+      {...props}
+      class="relative z-10 h-6 w-6 text-zinc-50 hover:bg-zinc-700 hover:text-zinc-50"
+      onClick$={() => {
+        copyToClipboardWithMeta(code);
+        hasCopied.value = true;
+      }}
+    >
+      <span class="sr-only">Copy</span>
+      {hasCopied.value ? <CheckIcon /> : <CopyIcon />}
+    </button>
+  );
+});
