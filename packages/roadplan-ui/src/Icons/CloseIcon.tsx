823feb9fe998a63087d24e2018f022bafc610494
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-05-24 12:27:45 +0200

    feat(icons): add icons

---
+++ packages/roadplan-ui/src/Icons/CloseIcon.tsx
@@ -0,0 +1,20 @@
+import { component$ } from "@builder.io/qwik";
+
+export const CloseIcon = component$(() => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-10 w-10 rounded-xl stroke-black p-1 dark:stroke-white"
+      fill="none"
+      stroke-width="2.5"
+      viewBox="0 0 24 24"
+      aria-label="Close"
+    >
+      <path
+        d="M6 18L18 6M6 6l12 12"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  );
+});
---
+++ packages/roadplan-ui/src/Icons/ThemeDarkIcon.tsx
@@ -0,0 +1,19 @@
+import { component$ } from "@builder.io/qwik";
+
+export const ThemeDarkIcon = component$(() => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      aria-hidden="true"
+      role="img"
+      class="w- h-8 rounded-xl p-1"
+      viewBox="0 0 24 24"
+    >
+      <path
+        class="fill-black dark:fill-white"
+        d="M21.64 13a1 1 0 0 0-1.05-.14a8.05 8.05 0 0 1-3.37.73a8.15 8.15 0 0 1-8.14-8.1a8.59 8.59 0 0 1 .25-2A1 1 0 0 0 8 2.36a10.14 10.14 0 1 0 14 11.69a1 1 0 0 0-.36-1.05Zm-9.5 6.69A8.14 8.14 0 0 1 7.08 5.22v.27a10.15 10.15 0 0 0 10.14 10.14a9.79 9.79 0 0 0 2.1-.22a8.11 8.11 0 0 1-7.18 4.32Z"
+      ></path>
+    </svg>
+  );
+});
